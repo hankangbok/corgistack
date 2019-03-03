483310dd9e4073c8f737a7aab8c17c604d4b6fdf
Hannah Kang <kyouyatamax@gmail.com>
2019-03-03 02:54:52 -0800

    Finally got react to render - webpack, parcel errors with WSL, so I pulled over the react scripts

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'; 
+import {render} from 'react-dom';
+
+render(
+  <div>
+    <h1>Corgi Stack from React</h1>
+    <p>IT FINALLY RENDERED</p>
+  </div>,
+  document.getElementById('root') 
+);
